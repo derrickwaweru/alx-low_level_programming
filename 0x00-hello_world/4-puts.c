9df7f416c8eafa4a7c3ef25cc0038df0d3b10982
Derrick Waweru <derrickwaweru38@gmail.com>
2022-12-08 07:06:08 -0800

    add

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 
 #include <stdio.h>
+/*This is the main function*/
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts(""Programming is like building a multilingual puzzle\n");
 	return (0);
 }
