d74acfa47e3a11d962ad96b92997533789376e47
Derrick Waweru <derrickwaweru38@gmail.com>
2022-12-08 11:22:38 -0800

    changed the %lu

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,11 @@
 
 int main(void)
 {
-	printf("Size of a char: %d", sizeof(char));
-	printf("Size of an int: %d", sizeof(int));
-	printf("Size of a long int: %d", sizeof(long int));
-	printf("Size of a long long int: %"d, sizeof(double));
-	printf("Size of a float: %d", sizeof(float));
+	printf("Size of a char: %lu", sizeof(char));
+	printf("Size of an int: %lu", sizeof(int));
+	printf("Size of a long int: %lu", sizeof(long int));
+	printf("Size of a long long int: %lu", sizeof(double));
+	printf("Size of a float: %lu", sizeof(float));
 
 	return (0);
 }
