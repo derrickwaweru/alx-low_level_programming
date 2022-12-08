cfede6c5058ffb8a429959c145c4c5af7adf72ea
Derrick Waweru <derrickwaweru38@gmail.com>
2022-12-08 07:34:21 -0800

    changed the comment format

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 
 #include <stdio.h>
-/*this ithe main function*/
+/**
+ * main - entry point
+ * description - a simple puts program
+ * this ithe main function
+ * return  - (0)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
