3d9bf35e42a6be60d6a04821be10a55ab07b0b9f
Derrick Waweru <derrickwaweru38@gmail.com>
2022-12-09 09:06:55 -0800

    create a new loop file

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+/* more headers goes there */
+
+
+
+/**
+ * main - program entry
+ * Description: This is a program to print positives and negatives
+ * Return: Always 0 (Success) */
+
+int main(void)
+
+{
+
+		int n;
+
+
+
+			srand(time(0));
+
+				n = rand() - RAND_MAX / 2;
+
+					if(n > 0)
+					{
+						printf("%d is positive \n", n);
+					}
+					if(n == 0)
+					{
+						printf("%d is zero \n", n);
+					}
+					if(n < 0)
+					{
+						prinf("%d is negative \n", n);
+					}
+
+					return (0);
+
+}
