1265531a50e57a428d2bd576e693d03e824e79fa
Derrick Waweru <derrickwaweru38@gmail.com>
2022-12-14 09:45:45 -0800

    addded space

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- * main - function prints alphabet in small letters
  * print_alphabet- prints the alphabet in lowercase then a new line
  */
 
 void print_alphabet(void)
 {
 	char letter;
+
 	for (letter = 'a'; letter <= 'z'; letter++)
+
 		_putchar(letter);
 
 	_putchar('\n');
