f4d72c8b804792f62d1e1c30aad843282daa1be7
Derrick Waweru <derrickwaweru38@gmail.com>
2022-12-15 09:07:38 -0800

    removed space before parenthesis

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * _isalpha: checks for uppercase characters
+ * _isalpha- checks for uppercase characters
  * @c: character to be checked
+ *
  * Return: Return 1 if uppercase, 0 otherwise
  */
 
 int _isalpha(int c)
 {
-	if (( c >= 'A' && c <= 'Z') ||
-	   ( c >= 'a' && c <= 'z'))
+	if ((c >= 'A' && c <= 'Z') ||
+	   (c >= 'a' && c <= 'z'))
 		return (1);
 
 	else
