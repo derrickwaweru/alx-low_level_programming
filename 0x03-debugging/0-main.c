ecd0e8b4db2439bf1a95e43a6e1299cb2d60407d
Derrick Waweru <derrickwaweru38@gmail.com>
2022-12-16 09:23:43 -0800

    added a debugging fn

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints whether the integer is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	
+	i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+
+}
