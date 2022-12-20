2945b8324f306edac5ea49f4f88cecc6863d2f78
Derrick Waweru <derrickwaweru38@gmail.com>
2022-12-20 10:18:02 -0800

    len of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - function to print the length of a string
+ *
+ * Return: Always 0
+ */
+
+int _strlen(char *s)
+
+{
+	int a = 0;
+
+	for(; *s++;)
+
+		a++;
+
+	return (a);
+
+}
